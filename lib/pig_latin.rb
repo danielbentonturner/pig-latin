184841baf01656ad9277c2351ab7a0a27b177968
daniel <demise4u@gmail.com>
2014-07-29 09:53:41 -0500

    initial gem commit

---
+++ lib/pig_latin.rb
@@ -0,0 +1,21 @@
+require "pig_latin/version"
+
+module PigLatin
+  def self.translate(word)
+    word = word.to_s.downcase
+    if word.match(/\d/) || word.match(/\W/)
+      :error
+      # raise "You must enter a single word, with no spaces, special characters, or numbers."
+    elsif word.match(/^[y]/)
+      "#{word[1..-1]}yay"
+    elsif word.match(/^[aeiou]/)
+      "#{word}way"
+    elsif word.match(/^[qu]{2}/)
+      start = word.slice!(/^[qu]{2}/)
+      "#{word}#{start}ay"
+    else
+      start = word.slice!(/^[^aeiouy]{1,}/)
+      "#{word}#{start}ay"
+    end
+  end
+end
